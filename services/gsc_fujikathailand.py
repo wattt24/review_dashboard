9e86436443fcbd44004c548a17fb90a41f53f719
watsana-data <watdataall@gmail.com>
2025-08-29 09:36:52 +0700

    dashboard123456

--- services/gsc_fujikathailand.py
+++ services/gsc_fujikathailand.py
@@ -7,7 +7,7 @@ import os
 
 SCOPES = ["https://www.googleapis.com/auth/webmasters.readonly"]
 
-service_account_info = json.loads(os.environ["service_account"])
+service_account_info = json.loads(os.environ["SERVICE_AC"])
 credentials = service_account.Credentials.from_service_account_info(
     service_account_info, scopes=SCOPES
 )
