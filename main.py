d0384c5fefe01fbdce17a7794137396ea27698f1
watsana-data <watdataall@gmail.com>
2025-09-02 13:18:09 +0700

    das11111

---
+++ main.py
@@ -0,0 +1,7 @@
+from services.shopee_auth import get_token
+
+code = "59487941577441765072796f4a616445"  # code ที่ได้จาก Shopee
+shop_id = 225734279                          # shop_id ของร้านค้า
+
+token_response = get_token(code, shop_id)
+print(token_response)
